cc5bee0490daaf5ed6a2de11c4dd5588d6aa3505
keung67 <127957412+keung67@users.noreply.github.com>
2025-01-16 10:06:18 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -2,7 +2,10 @@ import { serve } from "https://deno.land/std@0.218.0/http/server.ts";
 
 // 設定 URL
 const urls = [
- 'https://new-api-latest-4gv0.onrender.com/about'
+ // 'https://new-api-latest-4gv0.onrender.com/about',
+ 'https://new-api-latest-4gv0.onrender.com/api/notice',
+ 'https://new-api-latest-4gv0.onrender.com/api/status',
+ 'https://new-api-latest-4gv0.onrender.com/api/home_page_content'
 ];
 
 const websites = [];
